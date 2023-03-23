3c0a8a350191d4d755f81a00b8756fdcd8cf8cb1
maddyRanjha <ranjhamaddheshia01@gmail.com>
2023-03-24 01:37:22 +0530

    No Change

--- models/tasks.js
+++ models/tasks.js
@@ -8,22 +8,13 @@ const taskSchema = new mongoose.Schema({
   name: { type: String, required: true },
   desc: { type: String, required: true },
   date: { type: String, required: true },
-  noti: { type: Boolean, required: true},
+  editNoti: { type: Boolean, required: true},
+  editTask: { type: Boolean, required: true},
   notification: { type: String, required: true },
   frequency: { type: String, required: true },
   user: { type: mongoose.Schema.ObjectId, ref: 'users', required: true },
 });
 
 const Task = mongoose.model('tasks', taskSchema);
-
-// const validate = (data) => {
-// 	const schema = Joi.object({
-// 		firstName: Joi.string().required().label("First Name"),
-// 		lastName: Joi.string().required().label("Last Name"),
-// 		email: Joi.string().email().required().label("Email"),
-// 		password: passwordComplexity().required().label("Password"),
-// 	});
-// 	return schema.validate(data);
-// };
 
 module.exports = { Task };
